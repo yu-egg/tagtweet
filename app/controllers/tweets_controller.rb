8e0858e961e01ec1761e9933902f1deec85e2425
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-07 15:11:17 +0900

    コントローラー編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,12 @@ class TweetsController < ApplicationController
     end
   end
 
+  def search
+    return nil if params[:input] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:input]}%"] )
+    render json:{ keyword: tag }
+  end
+
   private
 
   def tweet_params
