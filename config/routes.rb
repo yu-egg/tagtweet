f8732786c9b8c2007f6cc46f5d2e1e80890b5091
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-07 15:07:22 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'tweets#index'
-  resources :tweets, only: [:new, :create]
+  resources :tweets, only: [:index, :new, :create] do
+    collection do
+      get 'search'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
