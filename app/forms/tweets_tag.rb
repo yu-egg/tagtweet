0c4b3ae8670e9fd2191f7da4fb4fad506ea58ed0
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-07 14:42:52 +0900

    「既存のレコードが存在する場合は、既存のレコードを反映させたい」という場合は、first_or_initializeを使用します。

--- app/forms/tweets_tag.rb
+++ app/forms/tweets_tag.rb
@@ -10,10 +10,13 @@ class TweetsTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize #「first_or_initialize」は、whereメソッドと一緒に使います。whereで検索した条件のレコードがあればそのレコードのインスタンスを返し、なければ新しくインスタンスを作るメソッドです。
+    tag.save
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
 
 end
-#Formオブジェクトと中間テーブルを用いる際は、中間テーブルにレコードを作成する記述が必要になります。バリデーションも一緒に記述しましょう。
+# Formオブジェクトと中間テーブルを用いる際は、中間テーブルにレコードを作成する記述が必要になります。バリデーションも一緒に記述しましょう。
+# 現状のアプリケーションでは、すでにデータベースに存在する文字列のタグは保存できません。tagsテーブルのnameカラムに一意性の制約を設けているためです。この場合、「tweet」に紐づく「tag」が生成されないため、タグ付け機能としては成り立ちません。
+# このように「既存のレコードが存在する場合は、既存のレコードを反映させたい」という場合は、first_or_initializeを使用します。
